e70794751cb14efb669e363f0c6906fa3a9c891c
tanveerisonline <tanveerisonline@gmail.com>
2022-12-22 01:41:53 +0530

    Add leaderboard project to github

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+function refresh() {
+  // Code to refresh the leaderboard data goes here
+}
+refresh();
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+
+  // Add the data to the leaderboard
+  const leaderboard = document.getElementById('leaderboard');
+  leaderboard.innerHTML += `<div>${name}: ${score}</div>`;
+});
