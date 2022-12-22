2c969bf000c08190c6cb807b93628c2e487fd139
tanveerisonline <tanveerisonline@gmail.com>
2022-12-23 02:29:56 +0530

    Add changes as per code reviewer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
 import './style.css';
-import { getScores, setScores } from './modules/api.js';
+import { createGame, post, get } from './modules/api.js';
 
-const inputForm = document.querySelector('#input-form');
-const user = document.querySelector('#inputName');
-const score = document.querySelector('#inputScore');
-inputForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const obj = {};
-  obj.user = user.value;
-  obj.score = score.value;
-  setScores(obj);
-  inputForm.reset();
-});
+const form = document.forms[0];
+const leaderboard = document.querySelector('#leaderboard');
 
-const render = async () => {
-  const list = await getScores();
-  const scoreList = document.querySelector('#leaderboard');
-  scoreList.innerHTML = '';
-  list.forEach((obj) => {
-    scoreList.innerHTML += `<div class="change-style">${obj.user}: ${obj.score}</div>`;
-  });
+// Save game id in local storage code
+const createId = async () => {
+  await createGame();
+  const ID = localStorage.getItem('Data');
+  return ID;
 };
-render();
+const id = localStorage.getItem('Data') || createId();
 
-const refreshList = document.querySelector('#refresh');
-refreshList.addEventListener('click', render);
+// post new player code
+const send = async () => {
+  const data = {
+    user: form[0].value,
+    score: form[1].value,
+  };
+  await post(id, data);
+};
+
+form[2].onclick = (e) => {
+  e.preventDefault();
+  if (form[0].value !== '' && form[1].value !== '') {
+    send();
+    form[0].value = '';
+    form[1].value = '';
+  } else {
+    form[0].placeholder = 'FILED CANT BE EMPLTY!!!';
+    form[1].placeholder = 'FILED CANT BE EMPLTY!!!';
+  }
+};
+
+// refresh code
+const refresh = async () => {
+  const data = await get(id);
+  if (data.length > 0) {
+    data.forEach((item) => {
+      const node = `
+      <li>
+        <p class='name change-style'>${item.user} : ${item.score}</p>
+      </li>`;
+      const child = document.createRange().createContextualFragment(node);
+      leaderboard.appendChild(child);
+    });
+  }
+};
+refresh();
+
+document.getElementById('refresh').onclick = () => {
+  leaderboard.innerHTML = '';
+  refresh();
+};
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,20 +1,37 @@
-const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/uVgJkr65ujFhWC2pGzJ/scores';
-
-const getScores = async () => {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data.result;
-};
-
-const setScores = async (obj) => {
-  const response = await fetch(`${apiUrl}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
+const createGame = () => {
+  const game = fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'My cool new game address' }),
     },
-    body: JSON.stringify(obj),
-  });
-  await response.json();
+  );
+  return game
+    .json()
+    .then((data) => localStorage.setItem('Data', /\w{20}/.exec(data.result)[0]));
 };
 
-export { getScores, setScores };
+const post = async (gameId, data) => {
+  await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    },
+  );
+};
+
+const get = async (gameId) => {
+  const score = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
+  );
+  return score.json().then((data) => data.result);
+};
+
+export { createGame, post, get };
